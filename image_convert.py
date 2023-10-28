7888435853b90b267bfe3a5e60ba30e1b77c91b7
stan.hordon <stan.hordon@gmail.com>
2023-10-28 12:04:32 -0700

    change comments

--- image_convert.py
+++ image_convert.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 folder_path = "D:\\movavi video"
 
-# Получение списка файлов в указанной папке
+# Reciving files from the original folder
 file_list = os.listdir(folder_path)
 
 #create folder for new files
@@ -11,7 +11,7 @@ folder = "D:\\movavi video\\opt\\icons\\"
 
 if not os.path.exists(folder):
     os.makedirs(folder)
-# Итерация по файлам
+# iteration over files
 for filename in file_list:
     if filename.endswith((".jpg", ".jpeg", ".png", ".gif")):
         image_path = os.path.join(folder_path, filename)
